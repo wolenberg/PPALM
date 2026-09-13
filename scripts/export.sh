6b80d648ce9b59cc0de113f6944ccbe7d25b23e5
wolenberg <wolembergjp@gmail.com>
2026-09-12 22:39:35 -0300

    validator included

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+echo "Exporting solution..."
+
 pac solution export \
+  --environment https://orgbc1db0d2.crm3.dynamics.com \
   --name PPALMCore \
   --path ./exports/PPALMCore.zip
+
+echo "Unpacking solution..."
 
 pac solution unpack \
   --zipfile ./exports/PPALMCore.zip \
   --folder ./solutions/PPALMCore
+
+echo "Done!"
